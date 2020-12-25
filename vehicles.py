f5db3af524b4cb127d6ce275784f4e0b29556469
Alvaro Rodriguez <alvaro.rodriguezdelgado@gmail.com>
2020-12-25 23:25:40 +0100

    script finished

--- vehicles.py
+++ vehicles.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 
 def  my_func(x):
-	return print(x.shape)
-
+	print(f'Data frame shape is {x.shape}')
+	print(f'Data frame length is  {len(x.T.columns.tolist())}')
+	print(f'Columns are: {x.columns}')
+	return
 #Example:
 vehicles_df=pd.read_excel('data/vehicles.xlsx')
 my_func(vehicles_df)
